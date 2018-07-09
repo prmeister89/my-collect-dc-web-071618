deb20f9a927b33e768ad10d852864722c1e73a0a
Peter Meister <prmeister89@gmail.com>
2018-07-09 22:30:14 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,8 @@ def my_collect(array)
   new_array = []
   
   while i < array.length
-    yield(array[i])
+    new_array << yield(array[i])
     i+=1
-    new_array << array[i]
   end
   return new_array
 end
